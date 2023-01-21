1ab13c612ca2521b61f600586c245dea4ae5cf90
Paiva <danpaiva@live.com>
2023-01-21 15:50:31 -0300

    feat: Add labels and button

--- PruuImageDownloader/PruuImageDownloader/Form1.Designer.cs
+++ PruuImageDownloader/PruuImageDownloader/Form1.Designer.cs
@@ -30,6 +30,15 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(F_Principal));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_product = new System.Windows.Forms.Label();
+            this.lbl_version = new System.Windows.Forms.Label();
+            this.lbl_arch = new System.Windows.Forms.Label();
+            this.btn_start_download = new System.Windows.Forms.Button();
+            this.lbl_language = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,11 +52,111 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // lbl_product
+            // 
+            this.lbl_product.AutoSize = true;
+            this.lbl_product.Location = new System.Drawing.Point(305, 48);
+            this.lbl_product.Name = "lbl_product";
+            this.lbl_product.Size = new System.Drawing.Size(55, 14);
+            this.lbl_product.TabIndex = 1;
+            this.lbl_product.Text = "Product";
+            // 
+            // lbl_version
+            // 
+            this.lbl_version.AutoSize = true;
+            this.lbl_version.Location = new System.Drawing.Point(305, 91);
+            this.lbl_version.Name = "lbl_version";
+            this.lbl_version.Size = new System.Drawing.Size(53, 14);
+            this.lbl_version.TabIndex = 2;
+            this.lbl_version.Text = "Version";
+            // 
+            // lbl_arch
+            // 
+            this.lbl_arch.AutoSize = true;
+            this.lbl_arch.Location = new System.Drawing.Point(305, 174);
+            this.lbl_arch.Name = "lbl_arch";
+            this.lbl_arch.Size = new System.Drawing.Size(82, 14);
+            this.lbl_arch.TabIndex = 3;
+            this.lbl_arch.Text = "Architecture";
+            // 
+            // btn_start_download
+            // 
+            this.btn_start_download.BackColor = System.Drawing.Color.SkyBlue;
+            this.btn_start_download.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_start_download.FlatAppearance.BorderSize = 0;
+            this.btn_start_download.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_start_download.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_start_download.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btn_start_download.Location = new System.Drawing.Point(394, 216);
+            this.btn_start_download.Name = "btn_start_download";
+            this.btn_start_download.Size = new System.Drawing.Size(170, 30);
+            this.btn_start_download.TabIndex = 4;
+            this.btn_start_download.Text = "START DOWNLOAD";
+            this.btn_start_download.UseVisualStyleBackColor = false;
+            // 
+            // lbl_language
+            // 
+            this.lbl_language.AutoSize = true;
+            this.lbl_language.Location = new System.Drawing.Point(305, 131);
+            this.lbl_language.Name = "lbl_language";
+            this.lbl_language.Size = new System.Drawing.Size(70, 14);
+            this.lbl_language.TabIndex = 6;
+            this.lbl_language.Text = "Language";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Windows",
+            "Office"});
+            this.comboBox1.Location = new System.Drawing.Point(394, 40);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(239, 22);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(394, 83);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(239, 22);
+            this.comboBox2.TabIndex = 8;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(394, 123);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(239, 22);
+            this.comboBox3.TabIndex = 9;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(394, 166);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(239, 22);
+            this.comboBox4.TabIndex = 10;
+            // 
             // F_Principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(676, 272);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lbl_language);
+            this.Controls.Add(this.btn_start_download);
+            this.Controls.Add(this.lbl_arch);
+            this.Controls.Add(this.lbl_version);
+            this.Controls.Add(this.lbl_product);
             this.Controls.Add(this.pictureBox1);
             this.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -58,11 +167,21 @@
             this.Text = "Pruu Image Downloader";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private PictureBox pictureBox1;
+        private Label lbl_product;
+        private Label lbl_version;
+        private Label lbl_arch;
+        private Button btn_start_download;
+        private Label lbl_language;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private ComboBox comboBox3;
+        private ComboBox comboBox4;
     }
 }
