6e9fb05e85cfa719bfb6d5aea94b33bce192f913
Paiva <danpaiva@live.com>
2023-04-06 21:54:21 -0300

    feat: some changes

--- PruuImageDownloader/PruuImageDownloader/F_Principal.cs
+++ PruuImageDownloader/PruuImageDownloader/F_Principal.cs
@@ -5,6 +5,7 @@ namespace PruuImageDownloader
 {
     public partial class F_Principal : Form
     {
+        const string edgeFolder = "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe";
         public F_Principal()
         {
             InitializeComponent();
@@ -14,14 +15,12 @@ namespace PruuImageDownloader
         {
             if (cb_product.Text == "Windows")
             {
-                cb_version.Items.Clear();
-                cb_version.Enabled = true;
+                CleanProduct();
                 cb_version.Items.Add("Windows 11 22H2");
             }
             else if (cb_product.Text == "Office")
             {
-                cb_version.Items.Clear();
-                cb_version.Enabled = true;
+                CleanProduct();
                 cb_version.Items.Add("Office 2021 LTSC");
             }
             else
@@ -32,14 +31,12 @@ namespace PruuImageDownloader
         {
             if (cb_version.Text == "Windows 11 22H2")
             {
-                cb_language.Items.Clear();
-                cb_language.Enabled = true;
+                CleanVersion();
                 cb_language.Items.Add("PT-BR");
             }
             else if (cb_version.Text == "Office 2021 LTSC")
             {
-                cb_language.Items.Clear();
-                cb_language.Enabled = true;
+                CleanVersion();
                 cb_language.Items.Add("PT-BR");
             }
             else
@@ -50,14 +47,12 @@ namespace PruuImageDownloader
         {
             if ((cb_language.Text == "PT-BR") && (cb_version.Text == "Windows 11 22H2"))
             {
-                cb_arch.Enabled = true;
-                cb_arch.Items.Clear();
+                CleanArch();
                 cb_arch.Items.Add("X64");
             }
             else if ((cb_language.Text == "PT-BR") && (cb_version.Text == "Office 2021 LTSC"))
             {
-                cb_arch.Enabled = true;
-                cb_arch.Items.Clear();
+                CleanArch();
                 cb_arch.Items.Add("X86");
                 cb_arch.Items.Add("X64");
             }
@@ -82,14 +77,38 @@ namespace PruuImageDownloader
         private void btn_start_download_Click(object sender, EventArgs e)
         {
             if(cb_product.Text == "Windows")
-                ValidateExecuteDownload.Windows_11_22h2_PtBr(cb_product, cb_version, cb_language, cb_arch);
+                ValidateExecuteDownload.Windows_11_22h2_PtBr(cb_product, cb_version, cb_language, cb_arch, edgeFolder);
             else
-                ValidateExecuteDownload.Office_2021_Pro_Plus_PtBr(cb_product, cb_version, cb_language, cb_arch);
+                ValidateExecuteDownload.Office_2021_Pro_Plus_PtBr(cb_product, cb_version, cb_language, cb_arch, edgeFolder);
         }
 
         private void lbl_contact_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             Process.Start(@"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe", "https://github.com/danhpaiva");
+        }
+
+        private void CleanProduct()
+        {
+            cb_version.Enabled = true;
+            cb_arch.Text = "";
+            cb_language.Text = "";
+            cb_version.Text = "";
+            cb_version.Items.Clear();
+        }
+
+        private void CleanVersion()
+        {
+            cb_language.Enabled = true;
+            cb_language.Text = "";
+            cb_version.Text = "";
+            cb_language.Items.Clear();
+        }
+
+        private void CleanArch()
+        {
+            cb_arch.Enabled = true;
+            cb_arch.Text = "";
+            cb_arch.Items.Clear();
         }
     }
 }
--- PruuImageDownloader/PruuImageDownloader/Helper/ValidateExecuteDownload.cs
+++ PruuImageDownloader/PruuImageDownloader/Helper/ValidateExecuteDownload.cs
@@ -4,7 +4,7 @@ namespace PruuImageDownloader.Helper
 {
     public static class ValidateExecuteDownload
     {
-        public static void Windows_11_22h2_PtBr(ComboBox cb_product, ComboBox cb_version, ComboBox cb_language, ComboBox cb_arch)
+        public static void Windows_11_22h2_PtBr(ComboBox cb_product, ComboBox cb_version, ComboBox cb_language, ComboBox cb_arch, string edgeFolder)
         {
             if (cb_product.Text == "Windows")
             {
@@ -13,14 +13,14 @@ namespace PruuImageDownloader.Helper
                     if (cb_language.Text == "PT-BR")
                     {
                         if (cb_arch.Text == "X64")
-                            Process.Start(@"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe", "http://dl.delivery.mp.microsoft.com/filestreamingservice/files/3eb153b9-dec7-4b38-bc69-189fef6e55ef/22621.525.220925-0207.ni_release_svc_refresh_CLIENTCONSUMER_RET_x64FRE_pt-br.esd");
+                            Process.Start($"{edgeFolder}", "http://dl.delivery.mp.microsoft.com/filestreamingservice/files/3eb153b9-dec7-4b38-bc69-189fef6e55ef/22621.525.220925-0207.ni_release_svc_refresh_CLIENTCONSUMER_RET_x64FRE_pt-br.esd");
                         else
                             MessageBox.Show("Sorry, image not available");
                     }
                 }
             }
         }
-        public static void Office_2021_Pro_Plus_PtBr(ComboBox cb_product, ComboBox cb_version, ComboBox cb_language, ComboBox cb_arch)
+        public static void Office_2021_Pro_Plus_PtBr(ComboBox cb_product, ComboBox cb_version, ComboBox cb_language, ComboBox cb_arch, string edgeFolder)
         {
             if (cb_product.Text == "Office")
             {
@@ -29,9 +29,9 @@ namespace PruuImageDownloader.Helper
                     if (cb_language.Text == "PT-BR")
                     {
                         if (cb_arch.Text == "X64")
-                            Process.Start(@"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe", "https://officecdn.microsoft.com/db/492350f6-3a01-4f97-b9c0-c7c6ddf67d60/media/pt-br/ProPlus2021Retail.img");
+                            Process.Start($"{edgeFolder}", "https://officecdn.microsoft.com/db/492350f6-3a01-4f97-b9c0-c7c6ddf67d60/media/pt-br/ProPlus2021Retail.img");
                         else
-                            Process.Start(@"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe", "https://officecdn.microsoft.com/db/492350f6-3a01-4f97-b9c0-c7c6ddf67d60/media/pt-br/ProPlus2021Retail.img");
+                            Process.Start($"{edgeFolder}", "https://officecdn.microsoft.com/db/492350f6-3a01-4f97-b9c0-c7c6ddf67d60/media/pt-br/ProPlus2021Retail.img");
                     }
                 }
             }
